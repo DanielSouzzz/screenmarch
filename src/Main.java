04d9097093c81efb41d173935081708a8f8a4f74
Daniel <danielsouzanovo@gmail.com>
2023-12-04 22:09:24 -0300

    private class

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Main {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O Poderoso Chefão";
-        meuFilme.anoDeLancamento = 1972;
-        meuFilme.incluidoNoPlano = true;
-        meuFilme.avaliacao = 4.7;
-        meuFilme.totalDeAvaliacoes = 5008;
-        meuFilme.duracaoEmMinutos = 177;
-
+        meuFilme.setNome("O poderoso Chefão");
+        meuFilme.setAnoDeLancamento(1972);
+        meuFilme.setDuracaoEmMinutos(177);
         meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(7);
+        meuFilme.avalia(8.3);
+
+        // System.out.println(meuFilme.somaDasAvaliacoes);
+        // System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.pegaMedia());
     }
 }
