64e00fb0729df01d8ae9853c04dd968951e7316e
Carlos Daniel de Souza Cordeiro <134225062+DanielSouzzz@users.noreply.github.com>
2024-02-11 23:33:17 -0300

    correções de erro.

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -14,18 +14,18 @@ public class PrincipalComListas {
         filmeDaCaroline.avalia(10);
         Filme outroFilme = new Filme("Avatar", 2023);
         outroFilme.avalia(7);
-        Serie lost = new Serie("Lost",2000);
+        Serie lost = new Serie("Lost", 2000);
 
         ArrayList<Titulo> lista = new ArrayList<>();
         lista.add(meuFilme);
         lista.add(filmeDaCaroline);
         lista.add(outroFilme);
         lista.add(lost);
-        for(Titulo item:lista){
+        for (Titulo item : lista) {
             System.out.println(item.getNome());
-            if ( item instanceof Filme filme && filme.getClassificacao() > 2){
-            System.out.println("Classificação "+filme.getClassificacao());
+            if (item instanceof Filme filme && filme.getClassificacao() > 2) {
+                System.out.println("Classificação " + filme.getClassificacao());
             }
-
+        }
     }
 }
