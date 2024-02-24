db2e83e8eca67b72466f9e53d9ee4b75c01e4b8c
Carlos Daniel de Souza Cordeiro <134225062+DanielSouzzz@users.noreply.github.com>
2024-02-23 21:37:32 -0300

    Implementei o scanner para permitir que o suário busque pelo filme desejado.

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -5,12 +5,19 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o nome do filme: ");
+        var busca = input.next();
+
+        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=971786e5";
+
     HttpClient client = HttpClient.newHttpClient();
     HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("http://www.omdbapi.com/?t=superman&apikey=971786e5"))
+            .uri(URI.create(endereco))
             .build();
     HttpResponse<String> response = client
             .send(request, HttpResponse.BodyHandlers.ofString());
